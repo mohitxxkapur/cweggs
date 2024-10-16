5651d7cd9563806ba71ec591d1321fc96ad9e220
Mohit Kapur <mkapur@datacable.ca>
2024-10-16 11:29:00 -0400

    more playing around

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-      
-      <div className="App-paragraph">
-        <p>faiz stinks b a d </p>
-      </div>
+		<header className="App-header">
+			<h1>Hello World!</h1>      
+			<div className="App-paragraph">
+				<p>faiz stinks b a d </p>
+			</div>
+		</header>
     </div>
   );
 }
