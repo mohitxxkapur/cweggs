dfc4b19301bda001b71e223a0d16a8631da65e1b
Mohit Kapur <mkapur@datacable.ca>
2024-10-16 11:17:55 -0400

    ??

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-//import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <header className="App-header">
+        <h1>Hello World!</h1>
+      </header>
+      
+      <div className="App-paragraph">
+        <p>faiz stinks</p>
+      </div>
     </div>
   );
 }
