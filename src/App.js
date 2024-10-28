d7311ed8d18ba0927c690ba9ce13d8a7ccc678c3
Mohit Kapur <mkapur@datacable.ca>
2024-10-28 13:17:18 -0400

    finally added checkbox and changed some styling - lots to do still

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
+import React from 'react';
 //test
-//sparkle sparkle
 
 const App = () => {
+	const [checked, setChecked] = React.useState(false); 
+	function handleChange(e) {
+	   setChecked(e.target.checked);
+	}
+
 //function App() {
   return (
     <div className="App">
 		<header className="App-header">
 			<h1>Hello World!</h1>      
 			<div className="App-subheader">
-				<p>faiz stinks b a d </p>
-				<p>this is a subheader </p>
+				<p>I am a subheader, about to introduce a checkbox </p>
 			</div>
 		</header>
+
+		<div className='checkbox-container'>
+			<input value = "test" type = "checkbox" onChange = {handleChange} /> 
+			<span>box</span>
+
+			<div className="result">
+        		<br></br>
+				<br></br>
+				Above checkbox is {checked ? "checked" : "un-checked"}.
+      		</div>
+		</div>
 		
-		
-		<div className="App-paragraph">
+		<div className="App-tinyparagraph">
 			<p>this is another paragraph </p>
       </div>
     </div>
